250e193729ffd306c4b3a991dce89be600f9001f
yaohao <291500530@qq.com>
2020-06-17 18:23:27 +0800

    first upload

---
+++ assets/scripts/welcome.js
@@ -0,0 +1,53 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        myGround:{
+            type:cc.Node,
+            default:null
+        },
+        myLoading:{
+            type:cc.Prefab,
+            default:null
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.myLoading = cc.instantiate(this.myLoading);
+        cc.log(this.myGround);
+        this.myGround.addChild(this.myLoading);
+        this.myLoading.y = 0;
+        var loading = this.myLoading.getComponent("loading");
+        loading.setLoadingItemWidth(0.2);
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
